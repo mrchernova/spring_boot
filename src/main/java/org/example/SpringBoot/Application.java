aee13b6c935dbf4347042d52fdd8483b1e8ee72a
mrchernova <cherraniram@gmail.com>
2022-12-07 17:26:00 +0300

    spring.boot

---
+++ src/main/java/org/example/SpringBoot/Application.java
@@ -0,0 +1,14 @@
+package org.example.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+
+https://www.youtube.com/watch?v=jH17YkBTpI4&list=PLU2ftbIeotGpAYRP9Iv2KLIwK36-o_qYk&ab_channel=letsCode
