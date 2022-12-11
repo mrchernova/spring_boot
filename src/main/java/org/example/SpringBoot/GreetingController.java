b13811901a8a7fb4be680a0c30aa15bea591bb0b
cherraniram@gmail.com <cherraniram@gmail.com>
2022-12-11 23:02:47 +0300

    thymeleaf tags

--- src/main/java/org/example/SpringBoot/GreetingController.java
+++ src/main/java/org/example/SpringBoot/GreetingController.java
@@ -1,19 +1,24 @@
 package org.example.SpringBoot;
 
+import org.example.SpringBoot.model.Message;
+import org.example.SpringBoot.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Iterator;
 import java.util.Map;
-import java.util.Objects;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepository mr;
 
     @GetMapping("/greeting")
     public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
 //1            Model model
             Map<String, Object> model
     ) {
@@ -23,9 +28,22 @@ public class GreetingController {
     }
 
     @GetMapping
-    public String main(Map<String, Object> model){
-        model.put("some", "Obama");
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = mr.findAll();
+        model.put("messages", messages);
         return "main";
     }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        mr.save(message);
+
+        Iterable<Message> messages = mr.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
 
 }
